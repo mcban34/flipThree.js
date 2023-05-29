e6a451fc17502e9fefda2da8bbf708e0196d40cb
mehmet çoban <74604824+mcban34@users.noreply.github.com>
2023-05-29 08:51:02 +0300

    update3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = 3000;
 
-app.use(express.static('public));
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Sunucu çalışıyor: http://localhost:${port}`);
